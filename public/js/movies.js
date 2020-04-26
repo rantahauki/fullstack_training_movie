df5f0049c326b37a785ec318b858d2437b842302
Matti Raunio <matti.raunio@gmail.com>
2020-04-26 17:49:57 +0300

    rest interface added

--- public/js/movies.js
+++ public/js/movies.js
@@ -1,3 +1,35 @@
+var movie_title = "";
+
+get_reviews();
+
+
+async function get_reviews(){
+  const response = await fetch("/api/reviews"); //get data from own server api
+  const data = await response.json();
+  console.log(data);
+  fill_the_review_table(data);
+}
+
+function fill_the_review_table(data) {
+    // Find a <table> element with id="myTable":
+  var table = document.getElementById("review_table");
+
+  for (var i = 0; i < data.length; i++) {
+
+    var row = table.insertRow(i+1);
+
+    // Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+
+    // Add some text to the new cells:
+    cell1.innerHTML = data[i].leffa;
+    cell2.innerHTML = data[i].arvostelu;
+    cell3.innerHTML = data[i].arvostelija;
+  }
+
+}
 
 async function get_movie() {
   var movie_name = document.getElementById("movie").value;
@@ -13,4 +45,38 @@ async function get_movie() {
   document.getElementById("movie_name").innerHTML = data.Title;
   poster.src = data.Poster; //add image
 
+  movie_title = data.Title;
+}
+
+
+async function review_movie() {
+  var leffa = movie_title;//document.getElementById("movie").value;
+  var arvostelu = document.getElementById("review").value;
+  var arvostelija = document.getElementById("reviewer").value;
+  const data = {leffa, arvostelu, arvostelija};
+  const options = {
+        method: "POST",
+        headers: {
+            "Content-Type":"application/json"
+        },
+        body: JSON.stringify(data)
+  };
+  fetch('/api/review',options).then(function(response) {
+    if(response.status = 200) {
+      console.log("OK");
+      empty_review_table();
+      get_reviews();
+    }
+  }, function (error){
+      console.log(error.message);
+  });
+}
+
+async function empty_review_table() {
+  var table = document.getElementById("review_table");
+  var row_count = table.rows.length - 1;
+
+  for (var i = 0; i < row_count; i++) {
+      table.deleteRow(1);
+  }
 }
--- server.js
+++ server.js
@@ -1,8 +1,36 @@
 //console.log("Hello world!");
 
+
+
 const express = require('express')
 const app = express();
 //app.listen(3000,() => console.log("Listening"));
 const port = process.env.PORT || 3000; //HEROKU settings -> it will read env otherwise use port 3000
 app.listen(port,() => console.log("Listening port:" + port));
 app.use(express.static("public"));
+
+app.use(express.json({limit: '1mb'}));
+
+const review_data = [
+    {
+        "leffa": "Titanic",
+        "arvostelu": "Klassikkokamaa",
+        "arvostelija": "Suvi"
+    },
+    {
+        "leffa": "Star Wars",
+        "arvostelu": "Silmäkarkkia",
+        "arvostelija": "Suvi 2"
+    }
+]
+
+app.get('/api/reviews',function (request, response) {
+  response.send(review_data);
+})
+
+app.post('/api/review',function (request, response) {
+  console.log(request.body);
+  review_data.push(request.body);
+  response.send(200);
+  console.log(review_data)
+})
